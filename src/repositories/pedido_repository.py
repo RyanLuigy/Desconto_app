42bacf73a9968f1bb98c2c544f885f41f011b0ae
RyanLuigy <ryan.luz@aln.senaicimatec.edu.br>
2026-05-05 13:24:58 -0300

    feat: adiciona arquitetura em camadas: implementação de todas as camadas

---
+++ src/repositories/pedido_repository.py
@@ -0,0 +1,14 @@
+from src.database.connection import DatabaseConnection
+from src.models.pedido import Pedido
+
+class PedidoRepository:
+    """Classe de repositório para armazenar e gerenciar os pedidos."""
+
+    def __init__(self, database: DatabaseConnection):
+        self.database = database
+
+    def adicionar_pedido(self, pedido: Pedido):
+        self.database.pedidos.append(pedido)
+
+    def listar_pedidos(self):
+        return self.database.pedidos
--- src/services/pedido_service.py
+++ src/services/pedido_service.py
@@ -3,13 +3,14 @@ from src.models.pedido import Pedido
 class PedidoService:
     """Classe de serviço para lidar com a lógica de negócios relacionada a pedidos."""
 
-    def __init__(self):
-        self.pedidos = []
+    def __init__(self, repository):
+        self.repository = repository
 
     def adicionar_pedido(self, pedido: Pedido):
-        self.pedidos.append(pedido)
+        self.repository.adicionar_pedido(pedido)
 
     def processar_pedidos(self):
-        for pedido in self.pedidos:
+        pedidos = self.repository.listar_pedidos()
+        for pedido in pedidos:
             print(f"Cliente: {pedido.cliente}")
             print(f"Valor final a pagar: R$ {pedido.valor_final(pedido.valor_original)}")
